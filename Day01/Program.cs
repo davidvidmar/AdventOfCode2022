8245dcffe78f0de3e6f940da78792e6b0b2d7180
David <info@vidmar.net>
2022-12-01 22:17:30 +0100

    Day 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using static AdventOfCode2022.Utils;
 
 class Program
 {
+    // https://adventofcode.com/2022/day/1
     static void Main()
     {
         int result1 = 0;
@@ -13,8 +16,27 @@ class Program
         // Part 1
         StartPart(1);
 
-        var lines = ReadInputAsIntLines("input.txt").ToArray();
+        var lines = ReadInputAsLines("input.txt").ToArray();
 
+        var elves = new List<int>();
+        var sum = 0;
+        foreach (var line in lines)
+        {
+            if (line.Length == 0)
+            {
+                elves.Add(sum);
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+            else
+            {
+                sum += Convert.ToInt32(line);
+            }
+        }
+        if (sum != 0)
+            elves.Add(sum);
+
+        result1 = elves.Max();
         // ...
 
         EndPart(1, result1);
@@ -22,7 +44,7 @@ class Program
         // Part 2
         StartPart(2);
 
-        // ...
+        result2 = elves.OrderByDescending(x => x).Take(3).Sum();
 
         EndPart(2, result2);
     }
