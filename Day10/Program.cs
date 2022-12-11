bcca9afcc9d9c5ff9ea045187299e700cd4c2045
David <info@vidmar.net>
2022-12-11 01:05:34 +0100

    Day 10

---
+++ Day10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using static AdventOfCode2022.Utils;
+
+class Program
+{
+    // https://adventofcode.com/2022/day/9
+
+    static void Main()
+    {
+        int result1 = 0;
+        int result2 = 0;
+
+        StartDay(9);
+
+        // Part 1
+        StartPart(1);
+
+        var lines = ReadInputAsLines("input.txt");
+
+	   // ...
+
+        EndPart(1, result1);
+
+        // Part 2
+        StartPart(2);
+
+	   // ...
+
+        EndPart(2, result2);
+    }
+
+}
